3a91fed5fedef8eb67bc2860d61e3da2ab49acea
thiagoboneth <thiago.luis@mercadolivre.com>
2021-10-15 15:15:17 -0300

    Base do projeto implantado

---
+++ src/main/java/com/mercadolibre/demo/MeliFriosApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliFriosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeliFriosApplication.class, args);
+    }
+
+}
