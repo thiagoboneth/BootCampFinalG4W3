e408385299e133f688a107ada2a631e62eda1495
pedroallima <pedro.leao@mercadolivre.com>
2021-10-19 09:26:25 -0300

    adicionando novos models (Buyar, InboundOrder, Section, WareHouse)

---
+++ src/main/java/com/mercadolibre/demo/model/WareHouse.java
@@ -0,0 +1,23 @@
+package com.mercadolibre.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "ware_house")
+public class WareHouse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gera autoincrimento no banco de dados
+    @Column(name = "id_ware_house")
+    private Long id_ware_house;
+
+
+}
