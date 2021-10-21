7d1b0eb7bc078547a57ab0bbe1af3330a17620d4
Thiago <t_boneth@hotmail.com>
2021-10-21 17:57:00 -0300

    WareHouse DTO implementado

---
+++ src/main/java/com/mercadolibre/demo/dto/WareHouseDTO.java
@@ -0,0 +1,21 @@
+package com.mercadolibre.demo.dto;
+
+import com.mercadolibre.demo.model.WareHouse;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+public class WareHouseDTO {
+
+        @NotNull(message = "O nome do deposito não pode ser nulo")
+        @NotEmpty(message = "O nome do deposito não pode estar vázio")
+        @Column(name = "ware_house_name")
+        private String ware_house_name;
+
+    public WareHouse convertObject() {
+        return new WareHouse(ware_house_name);
+    }
+}
