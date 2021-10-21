4ad69f7f5c4b57bd204312bfdcdf60f3ab9ca83f
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-10-21 15:23:03 -0300

    Criado repository

---
+++ src/main/java/com/mercadolibre/demo/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.mercadolibre.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mercadolibre.demo.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
