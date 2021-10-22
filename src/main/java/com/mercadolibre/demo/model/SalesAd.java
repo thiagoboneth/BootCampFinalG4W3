c4021ae087518d61c91ad3dd36c34efb0c8643e2
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-10-22 12:56:13 -0300

    Criado classe SalesAd

--- src/main/java/com/mercadolibre/demo/model/SalesAd.java
+++ src/main/java/com/mercadolibre/demo/model/SalesAd.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 import javax.persistence.*;
 
 @AllArgsConstructor
@@ -14,28 +16,43 @@ import javax.persistence.*;
 @Builder
 @Entity
 @Table(name = "sales_ad")
+public class SalesAd implements Serializable {
 
-public class SalesAd {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gera autoincrimento no banco de dados
-    @Column(name = "idseller_products")
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idseller_products", nullable = false)
     private Long id;
-    @Column(name = "volume")
+	
+    @Column(name = "volume", nullable = false)
     private Float volume;
-    @Column(name = "minimum_temperature")
+    
+    @Column(name = "minimum_temperature", nullable = false)
     private Float minimumTemperature;
-    @Column(name = "maximum_temperature")
+    
+    @Column(name = "maximum_temperature", nullable = false)
     private Float maximumTemperature;
+    
     @Column(name = "price")
     private Double price;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "idseller")
+    @JoinColumn(name = "idseller", nullable = false)
     private Seller seller;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "idproduct")
+    @JoinColumn(name = "idproduct", nullable = false)
     private Product product;
 
+	public SalesAd(Float volume, Float minimumTemperature, Float maximumTemperature, Double price, Seller seller,
+			Product product) {
+		super();
+		this.volume = volume;
+		this.minimumTemperature = minimumTemperature;
+		this.maximumTemperature = maximumTemperature;
+		this.price = price;
+		this.seller = seller;
+		this.product = product;
+	}
 }
