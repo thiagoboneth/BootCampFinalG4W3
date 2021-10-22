0bda933e8898c6694adfb1f82442b28f73e2b280
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-10-21 22:26:30 -0300

    Criado a classe BatchStock

--- src/main/java/com/mercadolibre/demo/model/BatchStock.java
+++ src/main/java/com/mercadolibre/demo/model/BatchStock.java
@@ -1,5 +1,6 @@
 package com.mercadolibre.demo.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -16,37 +17,58 @@ import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
 @Builder
 @Entity
 @Table(name = "batch_stock")
-public class BatchStock {
-	
+public class BatchStock implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) // Gera autoincrimento no banco de dados
-	@Column(name = "idbatch_number")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idbatch_number", nullable = false)
 	private Long batchNumber;
-	@Column(name = "current_temperature")
+
+	@Column(name = "current_temperature", nullable = false)
 	private Float currentTemperature;
-	@Column(name = "minimum_temperature")
+
+	@Column(name = "minimum_temperature", nullable = false)
 	private Float minimumTemperature;
-	@Column(name = "initial_quantity")
+
+	@Column(name = "initial_quantity", nullable = false)
 	private Long initialQuantity;
-	@Column(name = "current_quantity")
+
+	@Column(name = "current_quantity", nullable = false)
 	private Long currentQuantity;
-	@Column(name = "manufacturing_date")
+
+	@Column(name = "manufacturing_date", nullable = false)
 	private LocalDate manufacturingDate;
-	@Column(name = "manufacturing_time")
+
+	@Column(name = "manufacturing_time", nullable = false)
 	private LocalDateTime manufacturingTime;
+
 	@Column(name = "due_date")
 	private LocalDate dueDate;
 
 	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "idseller_products")
 	private SalesAd salesad;
+
+	public BatchStock(Float currentTemperature, Float minimumTemperature, Long initialQuantity, Long currentQuantity,
+			LocalDate manufacturingDate, LocalDateTime manufacturingTime, LocalDate dueDate, SalesAd salesad) {
+		this.currentTemperature = currentTemperature;
+		this.minimumTemperature = minimumTemperature;
+		this.initialQuantity = initialQuantity;
+		this.currentQuantity = currentQuantity;
+		this.manufacturingDate = manufacturingDate;
+		this.manufacturingTime = manufacturingTime;
+		this.dueDate = dueDate;
+		this.salesad = salesad;
+	}
 }
