9156e3ea5007c2afefa8715c1d2a23caeeb4d3a1
pedroallima <pedro.leao@mercadolivre.com>
2021-10-21 21:32:00 -0300

    DTO Buyer Service

---
+++ src/main/java/com/mercadolibre/demo/service/BuyerService.java
@@ -0,0 +1,39 @@
+package com.mercadolibre.demo.service;
+
+import com.mercadolibre.demo.model.Buyer;
+import com.mercadolibre.demo.model.Delegate;
+import com.mercadolibre.demo.repository.BuyerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class BuyerService {
+
+    private BuyerRepository buyerRepository;
+
+    @Autowired
+    public BuyerService(BuyerRepository buyerRepository) {
+        this.buyerRepository = buyerRepository;
+    }
+
+    // CREATE
+    public Buyer create(Buyer buyer) {
+        return buyerRepository.save(buyer);
+    }
+
+//    // READ
+//    public List<Delegate> list() {
+//        return wareHouseRepository.findAll();
+//    }
+//
+//    // UPDATE
+//    public WareHouse update(WareHouse wareHouse) {
+//        return wareHouseRepository.saveAndFlush(wareHouse);
+//    }
+//
+//     //DELETE
+//	public void delete(WareHouse wareHouse) {
+//        wareHouseRepository.deleteById(wareHouse.getId_ware_house());
+//	}
+}
