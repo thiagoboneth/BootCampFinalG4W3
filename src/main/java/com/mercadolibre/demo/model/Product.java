a815850b49b2d98c85b5cb3b3255178f2e0a9894
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-10-25 16:10:18 -0300

    Removido importacoes

--- src/main/java/com/mercadolibre/demo/model/Product.java
+++ src/main/java/com/mercadolibre/demo/model/Product.java
@@ -31,11 +31,9 @@ public class Product implements Serializable {
 	@Column(name = "name", nullable = false)
 	private String name;
 	@Column(name = "description", nullable = false)
-
 	private String description;
 	
 	
-	//Construtor
 	public Product(String name, String description) {
 		this.name = name;
 		this.description = description;
