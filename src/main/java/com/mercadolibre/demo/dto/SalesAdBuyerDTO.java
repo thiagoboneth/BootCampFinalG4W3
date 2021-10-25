945c3400e0e5e88ad3cc774677d6bede51a1e49b
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-10-25 16:09:02 -0300

    Removido importacoes dos DTOs

--- src/main/java/com/mercadolibre/demo/dto/SalesAdBuyerDTO.java
+++ src/main/java/com/mercadolibre/demo/dto/SalesAdBuyerDTO.java
@@ -3,16 +3,10 @@ package com.mercadolibre.demo.dto;
 import com.mercadolibre.demo.model.*;
 import lombok.Getter;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
-
 @Getter
 public class SalesAdBuyerDTO {
 
-
     private SalesAd salesad;
-
     private Buyer buyer;
 	
 	
