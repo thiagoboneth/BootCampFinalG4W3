34c33aa5b716273cc7dc6a4c4e5bdee713965662
Thiago <t_boneth@hotmail.com>
2021-10-26 17:46:18 -0300

    SalesAD implementa lista do carrinho

--- src/main/java/com/mercadolibre/demo/dto/WareHouseDTO.java
+++ src/main/java/com/mercadolibre/demo/dto/WareHouseDTO.java
@@ -10,10 +10,10 @@ import javax.validation.constraints.NotNull;
 @Getter
 public class WareHouseDTO {
 
-        @NotNull(message = "O nome do deposito não pode ser nulo")
-        @NotEmpty(message = "O nome do deposito não pode estar vázio")
-        @Column(name = "ware_house_name")
-        private String ware_house_name;
+    @NotNull(message = "O nome do deposito não pode ser nulo")
+    @NotEmpty(message = "O nome do deposito não pode estar vázio")
+    @Column(name = "ware_house_name")
+    private String ware_house_name;
 
     public WareHouse convertObject() {
         return new WareHouse(ware_house_name);
