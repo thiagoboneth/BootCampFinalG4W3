ce3661b7b49d1031982c1087a5adc5589dc73a84
Thiago <t_boneth@hotmail.com>
2021-10-26 17:46:18 -0300

    SalesAD implementa lista do carrinho

---
+++ src/main/java/com/mercadolibre/demo/advisor/Advisor.java
@@ -0,0 +1,18 @@
+package com.mercadolibre.demo.advisor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+//@ControllerAdvice
+public class Advisor extends ResponseEntityExceptionHandler{
+
+//    @ExceptionHandler(RuntimeException.class)
+//    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+//        String bodyOfResponse = ex.getMessage();
+//        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+//    }
+}
