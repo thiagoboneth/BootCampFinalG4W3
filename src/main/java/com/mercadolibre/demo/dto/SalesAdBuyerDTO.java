95d9142f2002e40da313cf0c55943503499957bd
Thiago <t_boneth@hotmail.com>
2021-10-27 11:09:46 -0300

    DTO corrigido para nova regra do de retorno com salesAD e quantity

--- src/main/java/com/mercadolibre/demo/dto/SalesAdBuyerDTO.java
+++ src/main/java/com/mercadolibre/demo/dto/SalesAdBuyerDTO.java
@@ -1,18 +1,19 @@
 package com.mercadolibre.demo.dto;
 
 import com.mercadolibre.demo.model.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
+@AllArgsConstructor
 @Getter
 public class SalesAdBuyerDTO {
 
-    private List<SalesAd> salesad;
-    private Buyer buyer;
-	
-	
-//	public SalesAdBuyer convertObject() {
-//		return new SalesAdBuyer(salesad, buyer);
-//	}
+	private SalesAd salesAd;
+	private int quantity;
+
+	public SalesAdBuyer convertObject() {
+		return new SalesAdBuyer(quantity, salesAd);
+	}
 }
