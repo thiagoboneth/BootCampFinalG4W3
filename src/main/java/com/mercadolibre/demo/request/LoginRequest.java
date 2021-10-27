e02bb1b5812b598e49c7845e6d69e887874b30ed
Thiago <t_boneth@hotmail.com>
2021-10-27 11:08:08 -0300

    Classe de login request implementado

---
+++ src/main/java/com/mercadolibre/demo/request/LoginRequest.java
@@ -0,0 +1,25 @@
+package com.mercadolibre.demo.request;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+public class LoginRequest {
+    private String user;
+    private String senha;
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public String getUser() {
+        return user;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public UsernamePasswordAuthenticationToken converter() {
+
+        return new UsernamePasswordAuthenticationToken(user, senha);
+    }
+}
