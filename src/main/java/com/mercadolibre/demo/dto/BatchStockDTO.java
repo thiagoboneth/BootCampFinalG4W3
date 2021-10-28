81f0f0bf6708b137d4820cb1f7e36a5d1a269141
MyMeliMail <isac.cordeiro@mercadolivre.com>
2021-10-28 14:12:25 -0300

    Refatoradas Classes BatchStock e BatchStockDTO

--- src/main/java/com/mercadolibre/demo/dto/BatchStockDTO.java
+++ src/main/java/com/mercadolibre/demo/dto/BatchStockDTO.java
@@ -2,52 +2,29 @@ package com.mercadolibre.demo.dto;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-
-import com.mercadolibre.demo.model.BatchStock;
-import com.mercadolibre.demo.model.SalesAd;
+import javax.validation.constraints.NotBlank;
 
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class BatchStockDTO {
 
-	@NotNull(message = "A temperatura atual não pode ser nula")
-	@NotEmpty (message = "A temperatura atual não pode ser vazia")
-	private Float currentTemperature;
+    @NotBlank(message = "{currentTemperature.not.blank}")
+    private Float currentTemperature;
+    @NotBlank(message = "{minimumTemperature.not.blank}")
+    private Float minimumTemperature;
+    @NotBlank(message = "{initialQuantity.not.blank}")
+    private Long initialQuantity;
+    @NotBlank(message = "{currentQuantity.not.blank}")
+    private Long currentQuantity;
+    @NotBlank(message = "{manufacturingDate.not.blank}")
+    private LocalDate manufacturingDate = LocalDate.now();
+    @NotBlank(message = "{manufacturingTime.not.blank}")
+    private LocalDateTime manufacturingTime = LocalDateTime.now();
+    @NotBlank(message = "{dueDate.not.blank}")
+    private LocalDate dueDate = LocalDate.now();
+    private Long salesadId;
 
-	@NotNull(message = "A temperatura mínima atual não pode ser nula")
-	@NotEmpty (message = "A temperatura mínima atual não pode estar vazia")
-	private Float minimumTemperature;
-
-	@NotNull(message = "A quantidade inicial não pode ser nula")
-	@NotEmpty (message = "A quantidade inicial não pode estar vazia")
-	private Long initialQuantity;
-
-	@NotNull(message = "A quantidade atual não pode ser nula")
-	@NotEmpty (message = "A quantidade atual não pode estar vázia")
-	private Long currentQuantity;
-
-	@NotNull(message = "A data de fabricacao não pode ser nula")
-	@NotEmpty (message = "A data de fabricacao não pode estar vázia")
-	private LocalDate manufacturingDate = LocalDate.now();
-
-	@NotNull(message = "O tempo de fabricacao não pode ser nulo")
-	@NotEmpty (message = "O tempo de fabricacao não pode estar vázio")
-	private LocalDateTime manufacturingTime = LocalDateTime.now();
-
-	@NotNull(message = "A data de vencimento não pode ser nula")
-	@NotEmpty (message = "A data de vencimento não pode estar vázia")
-	private LocalDate dueDate = LocalDate.now();
-
-	@NotNull(message = "O anúncio de vendas não pode ser nulo")
-	@NotEmpty (message = "O anúncio de vendas não pode estar vázio")
-	private SalesAd salesad;
-
-
-	public BatchStock convertObject() {
-		return new BatchStock(currentTemperature, minimumTemperature, initialQuantity,currentQuantity,
-				manufacturingDate, manufacturingTime, dueDate, salesad);
-	}
 }
