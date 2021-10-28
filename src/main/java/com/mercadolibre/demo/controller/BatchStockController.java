c037c20cdd286a745700a05196b82f341f7dd220
MyMeliMail <isac.cordeiro@mercadolivre.com>
2021-10-28 14:12:52 -0300

    Refatoradas Classes Seller e SallerDTO

--- src/main/java/com/mercadolibre/demo/controller/BatchStockController.java
+++ src/main/java/com/mercadolibre/demo/controller/BatchStockController.java
@@ -1,56 +1,62 @@
 package com.mercadolibre.demo.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.mercadolibre.demo.dto.BatchStockDTO;
-import com.mercadolibre.demo.dto.response.BatchStockResponseDTO;
 import com.mercadolibre.demo.model.BatchStock;
 import com.mercadolibre.demo.service.BatchStockService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/fresh-products/batchstock")
 public class BatchStockController {
-	
-	@Autowired
-	private BatchStockService batchStockService;
-	
-	@PostMapping(value = "/save")
-	public ResponseEntity<BatchStockResponseDTO> saveBatchStock(@RequestBody BatchStockDTO dto) {
-		BatchStock batchStock = batchStockService.save(dto.convertObject());
-		return new ResponseEntity<>(BatchStockResponseDTO.convertDTO(batchStock), HttpStatus.CREATED);
-	}
-	
-	@GetMapping(value = "/list")
-	@ResponseBody
-	public ResponseEntity<List<BatchStock>> listBatchStock() {
-		List<BatchStock> batchStocks = batchStockService.list();
-		return new ResponseEntity<>(batchStocks, HttpStatus.OK);	
-	}
-	
-	@PutMapping(value = "/update")
-	@ResponseBody
-	public ResponseEntity<BatchStock> updateBatchStock(@RequestBody BatchStock batchStock) {
-		BatchStock b = batchStockService.update(batchStock);
-		return new ResponseEntity<>(b, HttpStatus.CREATED);
-	}
-	
-	@DeleteMapping(value = "/delete")
-	@ResponseBody
-	public ResponseEntity<String> deleteBatchStock(@RequestParam Long batchNumber) {
-		batchStockService.delete(batchNumber);
-		return new ResponseEntity<>("Product successfully deleted", HttpStatus.OK);
-	}
+
+    @Autowired
+    private BatchStockService batchStockService;
+
+    @PostMapping(value = "/save")
+    public ResponseEntity<BatchStock> saveBatchStock(@Valid @RequestBody BatchStockDTO dto) throws Exception {
+        try {
+            BatchStock batchStock = batchStockService.save(dto);
+            return new ResponseEntity<>(batchStock, HttpStatus.CREATED);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping(value = "/list")
+    @ResponseBody
+    public ResponseEntity<List<BatchStock>> listBatchStock() {
+        List<BatchStock> batchStocks = batchStockService.list();
+        return new ResponseEntity<>(batchStocks, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/update/{id}")
+    @ResponseBody
+    public ResponseEntity<BatchStock> updateSalesAd(@Valid @RequestBody BatchStockDTO dto, @PathVariable Long id) throws Exception {
+        try {
+            BatchStock batchStock = batchStockService.update(dto, id);
+            return new ResponseEntity<>(batchStock, HttpStatus.CREATED);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<String> deleteBatchStock(@PathVariable Long id) {
+        try {
+            batchStockService.delete(id);
+            return new ResponseEntity<>("BatchStock successfully deleted", HttpStatus.OK);
+
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
