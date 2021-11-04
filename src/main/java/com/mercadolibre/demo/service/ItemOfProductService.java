576b0e74d03b02dc47bc6400eab6091d16fad351
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-04 17:19:06 -0300

    Metodo de conversão de DTO e delete implementados

--- src/main/java/com/mercadolibre/demo/service/ItemOfProductService.java
+++ src/main/java/com/mercadolibre/demo/service/ItemOfProductService.java
@@ -1,10 +1,8 @@
 package com.mercadolibre.demo.service;
 
 
-import com.mercadolibre.demo.dto.ItemOfProduct2DTO;
 import com.mercadolibre.demo.dto.ItemOfProductDTO;
 import com.mercadolibre.demo.model.ItemOfProduct;
-import com.mercadolibre.demo.model.PurchaseOrder;
 import com.mercadolibre.demo.model.SalesAd;
 import com.mercadolibre.demo.repository.ItemOfProductRepository;
 import com.mercadolibre.demo.repository.PurchaseOrderRepository;
@@ -12,48 +10,56 @@ import com.mercadolibre.demo.repository.SalesAdRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ItemOfProductService {
 
-    @Autowired
     private ItemOfProductRepository itemOfProductRepository;
-    @Autowired
     private SalesAdRepository salesAdRepository;
-    @Autowired
     private PurchaseOrderRepository purchaseOrderRepository;
+
+    @Autowired
+    public ItemOfProductService(ItemOfProductRepository itemOfProductRepository, SalesAdRepository salesAdRepository, PurchaseOrderRepository purchaseOrderRepository) {
+        this.itemOfProductRepository = itemOfProductRepository;
+        this.salesAdRepository = salesAdRepository;
+        this.purchaseOrderRepository = purchaseOrderRepository;
+    }
 
     public ItemOfProduct save(ItemOfProductDTO dto) throws Exception {
         ItemOfProduct itemOfProduct = convertItemOfProduct(dto);
         return itemOfProductRepository.save(itemOfProduct);
     }
 
-/*    public ItemOfProduct convertItemOfProduct(Long id,ItemOfProductDTO dto) throws Exception {
-        Optional<SalesAd> salesAd = salesAdRepository.findById(dto.getSalesAd());
-        Optional<PurchaseOrder> purchaseOrder = purchaseOrderRepository.findById(dto.getPurchaseOrder());
-        if (salesAd.isPresent()){
-            // return new ItemOfProduct(dto.getQuantity(),salesAd, purchaseOrder);
-            return null;
-
-        }
-        throw new Exception("Erro no carrinho");
-    }*/
-    /*    public ItemOfProduct convertItemOfProduct(ItemOfProduct2DTO dto) throws Exception {
-        Optional<SalesAd> salesAd = salesAdRepository.findById(dto.getSalesAd());
-        if (salesAd.isPresent()){
-            return new ItemOfProduct(dto.getQuantity(),salesAd.get());
-
-        }
-        throw new Exception("Erro no carrinho");
-    }*/
     public ItemOfProduct convertItemOfProduct(ItemOfProductDTO dto) throws Exception {
         Optional<SalesAd> salesAd = salesAdRepository.findById(dto.getSalesAd());
-       // Optional<PurchaseOrder> purchaseOrder = purchaseOrderRepository.findById(dto.getPurchaseOrder());
         if (salesAd.isPresent()){
              return new ItemOfProduct(dto.getQuantity(),salesAd.get());
         }
         throw new Exception("Erro no carrinho");
     }
+
+    public List<ItemOfProductDTO> list(Long name) {
+        List<ItemOfProductDTO> itemOfProductDTOS;
+        return convertItemOfProductDTO(itemOfProductRepository.orderOfItem(name));
+    }
+
+    public List<ItemOfProductDTO> convertItemOfProductDTO(List<ItemOfProduct> list){
+        List<ItemOfProductDTO> itemOfProductDTOS = new ArrayList<>();
+        ItemOfProductDTO itemOfProductDTO = new ItemOfProductDTO();
+        for (ItemOfProduct item: list) {
+            itemOfProductDTO.setQuantity(item.getQuantity());
+            itemOfProductDTO.setSalesAd(item.getSalesAd().getId());
+            itemOfProductDTO.setNameProduct(item.getSalesAd().getProduct().getName());
+            itemOfProductDTOS.add(itemOfProductDTO);
+        }
+        return itemOfProductDTOS;
+    }
+
+    public void delete(Long id) {
+        itemOfProductRepository.deleteById(id);
+    }
+
 }
