0715992a51d9a5a6036a7b691c8ed6dab2f888c9
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-04 17:18:31 -0300

    Query de consulta no banco para lista

--- src/main/java/com/mercadolibre/demo/repository/ItemOfProductRepository.java
+++ src/main/java/com/mercadolibre/demo/repository/ItemOfProductRepository.java
@@ -1,11 +1,17 @@
 package com.mercadolibre.demo.repository;
 
 import com.mercadolibre.demo.model.ItemOfProduct;
+import com.mercadolibre.demo.model.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ItemOfProductRepository extends JpaRepository<ItemOfProduct, List<Long>> {
+public interface ItemOfProductRepository extends JpaRepository<ItemOfProduct, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT s.* from  item_of_product s where upper(trim(s.id_purchase_order)) = ?1")
+    List<ItemOfProduct> orderOfItem (Long name);
+
 }
