2150ba620c9caf8fdad00318a98bcca173faeb71
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-04 14:32:02 -0300

    dto section category dto

---
+++ src/main/java/com/mercadolibre/demo/dto/SectionCategoryDTO.java
@@ -0,0 +1,11 @@
+package com.mercadolibre.demo.dto;
+
+public class SectionCategoryDTO {
+
+    private Long section_code;
+    private Double price;
+    private String name;
+    private String category;
+    private String ware_house_name;
+
+}
