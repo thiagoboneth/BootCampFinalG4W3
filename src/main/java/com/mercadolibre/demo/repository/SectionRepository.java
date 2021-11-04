86f10087856a3cf7891272892fbec091d2b2159f
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-04 14:33:19 -0300

    Query final implementada de section

--- src/main/java/com/mercadolibre/demo/repository/SectionRepository.java
+++ src/main/java/com/mercadolibre/demo/repository/SectionRepository.java
@@ -1,5 +1,6 @@
 package com.mercadolibre.demo.repository;
 
+import com.mercadolibre.demo.dto.SectionCategoryDTO;
 import com.mercadolibre.demo.dto.SectionTypeDTO;
 import com.mercadolibre.demo.model.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,6 +16,10 @@ public interface SectionRepository extends JpaRepository<Section,Long> {
     //@Query(value = "SELECT s.sectionCode, s.capacity, s.category, s.idWareHouse.idWareHouse from ItemOfProduct iop, SalesAd sa, BatchStock bs, InboundOrder io, Section s ,Product p, WareHouse wh where iop.salesAd.id = sa.id AND sa.id = bs.salesad.id AND bs.batchNumber = io.batchStock.batchNumber AND io.section.sectionCode = s.sectionCode AND p.id = sa.product.id AND s.idWareHouse.idWareHouse = wh.idWareHouse AND  upper(trim(s.category)) like %?1%")
     @Query(nativeQuery = true, value = "SELECT s.* from  section s where upper(trim(s.category)) like %?1%")
     List<Section> buscarPorSessao(String name);
+
+    @Query(nativeQuery = true, value = "SELECT s.section_code, sa.price,p.name,s.category, wh.ware_house_name from sales_ad sa, batch_stock bs, inbound_order io, section s ,products p, ware_house wh where sa.idsales_ad = bs.idsales_ad AND bs.idbatch_number = io.idbatch_number AND io.section_code = s.section_code AND p.idproduct = sa.idproduct AND s.id_ware_house = wh.id_ware_house AND upper(trim(s.category)) like %?1%")
+    List<SectionCategoryDTO> listProductForCategory(String category);
+
 }
 
 //.category, iop.quantity, sa.price, wh.wareHouseName, p.name
