1ebcdccce053cbced8250f4c5886af5919f52568
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-11-05 18:49:38 -0300

    Adicionado AllArgsConstructor em Product

--- src/main/java/com/mercadolibre/demo/model/Product.java
+++ src/main/java/com/mercadolibre/demo/model/Product.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,6 +18,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "products")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
