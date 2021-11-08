de46b226ea0ece21cb07093443292a374d41f5cd
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-08 18:10:57 -0300

    Refatorando nome de variavel

--- src/main/java/com/mercadolibre/demo/service/PurchaseOrderService.java
+++ src/main/java/com/mercadolibre/demo/service/PurchaseOrderService.java
@@ -47,7 +47,7 @@ public class PurchaseOrderService {
     public PurchaseOrder convertPurchaseToDTO(PurchaseOrderDTO dto) throws Exception {
         PurchaseOrder purchaseOrder = new PurchaseOrder();
         Optional<Buyer> buyer = buyerRepository.findById(dto.getIdBuyer());
-        purchaseOrder.setBuyer(buyer.get());
+        purchaseOrder.setIdBuyer(buyer.get());
         if (buyer.isPresent()) {
             List<ItemOfProduct> itemOfProducts = convertItemOfProduct(dto.getItemOfProduct(), purchaseOrder);
             purchaseOrder.setItemOfProduct(itemOfProducts);
