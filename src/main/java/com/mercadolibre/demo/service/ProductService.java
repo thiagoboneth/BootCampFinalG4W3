f8a55e6cbbc7087653a8bc8827c93c51f26ccf99
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-10 17:47:12 -0300

    Metodo de validade de produto

--- src/main/java/com/mercadolibre/demo/service/ProductService.java
+++ src/main/java/com/mercadolibre/demo/service/ProductService.java
@@ -1,21 +1,32 @@
 package com.mercadolibre.demo.service;
 
-import java.util.List;
-import java.util.Optional;
+import com.mercadolibre.demo.dto.ProductDTO;
+import com.mercadolibre.demo.dto.response.DueDateDTO;
+import com.mercadolibre.demo.model.InboundOrder;
+import com.mercadolibre.demo.model.Product;
+import com.mercadolibre.demo.repository.InboundOrderRepository;
+import com.mercadolibre.demo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.mercadolibre.demo.dto.ProductDTO;
-import com.mercadolibre.demo.model.Product;
-import com.mercadolibre.demo.repository.ProductRepository;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.chrono.ChronoLocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
 
 	private ProductRepository productRepository;
+	private InboundOrderRepository inboundOrderRepository;
 
 	@Autowired
-	public ProductService(ProductRepository productRepository) {
+	public ProductService(ProductRepository productRepository, InboundOrderRepository inboundOrderRepository) {
 		this.productRepository = productRepository;
+		this.inboundOrderRepository = inboundOrderRepository;
 	}
 
 	public Product save(ProductDTO dto) {
@@ -53,5 +64,31 @@ public class ProductService {
 
 	public Product convertProductToDTO(ProductDTO dto) {
 		return new Product(dto.getName(), dto.getDescription());
+	}
+
+	public List<DueDateDTO> dueDate(Long numberOfDay, Long idSection){
+		List<InboundOrder> dueDateList = inboundOrderRepository.findAll();
+		List<DueDateDTO> dueDateDTOList = new ArrayList<>();
+		final LocalDate dataAtual = LocalDate.now();
+		//TODO: Kenyo
+		LocalDate naoSei = dataAtual.minusDays(numberOfDay);
+
+
+		for (InboundOrder itemInboundOrder : dueDateList){
+			DueDateDTO requisiteFive = new DueDateDTO();
+			requisiteFive.setBatchNumber(itemInboundOrder.getBatchStock().getIdBatchNumber());
+			requisiteFive.setProductId(itemInboundOrder.getBatchStock().getIdSalesAd().getProduct().getName());
+			requisiteFive.setProductTypeID(itemInboundOrder.getSection().getCategory());
+			requisiteFive.setDueDate(itemInboundOrder.getBatchStock().getDueDate());
+			requisiteFive.setQuantity(itemInboundOrder.getBatchStock().getCurrentQuantity());
+			dueDateDTOList.add(requisiteFive);
+		}
+		//TODO: Kenyo
+		List<DueDateDTO> result = dueDateDTOList.stream().filter(d -> d.getDueDate().isAfter(naoSei)).collect(Collectors.toList());
+		//dueDateDTOList.stream().filter( p -> p.getDueDate().isAfter(LocalDate.of(dataTeste))).collect(Collectors.toList());
+
+		Collections.sort(result, Comparator.comparing(DueDateDTO::getDueDate));
+
+		return result;
 	}
 }
