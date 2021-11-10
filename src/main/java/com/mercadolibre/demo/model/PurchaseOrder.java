6baf349a8c1a2e6f1ccf3b2207686e2bc9eb6492
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-11-10 18:44:26 -0300

    Removido anotacao desnecessária em PurchaseOrder

--- src/main/java/com/mercadolibre/demo/model/PurchaseOrder.java
+++ src/main/java/com/mercadolibre/demo/model/PurchaseOrder.java
@@ -21,7 +21,6 @@ public class PurchaseOrder implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_purchase_order")
