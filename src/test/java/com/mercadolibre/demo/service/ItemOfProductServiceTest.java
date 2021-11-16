9f769708e32fa67c7933713d86554fff9325e403
MyMeliMail <isac.cordeiro@mercadolivre.com>
2021-11-16 19:25:46 -0300

    Concluídos testes list e convertItemOfProductDTO de ItemOfProductService

--- src/test/java/com/mercadolibre/demo/service/ItemOfProductServiceTest.java
+++ src/test/java/com/mercadolibre/demo/service/ItemOfProductServiceTest.java
@@ -1,16 +1,18 @@
 package com.mercadolibre.demo.service;
 
 import com.mercadolibre.demo.dto.ItemOfProductDTO;
-import com.mercadolibre.demo.model.ItemOfProduct;
-import com.mercadolibre.demo.model.SalesAd;
+import com.mercadolibre.demo.model.*;
 import com.mercadolibre.demo.repository.*;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 public class ItemOfProductServiceTest {
 
@@ -33,7 +35,7 @@ public class ItemOfProductServiceTest {
         itemOfProductDTO.setNameProduct("Laranja");
         itemOfProductDTO.setQuantity(5000L);
 
-        ItemOfProduct itemOfProduct =  new ItemOfProduct();
+        ItemOfProduct itemOfProduct = new ItemOfProduct();
         SalesAd salesAd = new SalesAd();
         salesAd.setId(1L);
 
@@ -51,7 +53,94 @@ public class ItemOfProductServiceTest {
         assertNotNull(itemOfProduct.getQuantity());
         assertNotNull(itemOfProduct.getSalesAd());
         assertNotNull(itemOfProduct.getSalesAd().getId());
+    }
 
+    @Test
+    void testListItemOfProduct() {
+
+        List<ItemOfProduct> itemOfProducts = new ArrayList<>();
+
+        ItemOfProduct itemOfProduct = new ItemOfProduct();
+        itemOfProduct.setId(1L);
+        itemOfProducts.add(itemOfProduct);
+
+        ItemOfProduct itemOfProduct2 = new ItemOfProduct();
+        itemOfProduct2.setId(2L);
+        itemOfProducts.add(itemOfProduct2);
+
+        ItemOfProduct itemOfProduct3 = new ItemOfProduct();
+        itemOfProduct3.setId(1L);
+        itemOfProducts.add(itemOfProduct3);
+
+        when(mockItemOfProductRepository.findAll()).thenReturn(itemOfProducts);
+        Mockito.when(mockItemOfProductRepository.orderOfItem(1L)).thenReturn(itemOfProducts);
+        List<ItemOfProduct> listaObtida = mockItemOfProductRepository.orderOfItem(1L);
+        itemOfProductService.list(2L);
+
+        assertNotNull(listaObtida);
+        assertTrue(listaObtida.contains(itemOfProduct));
+        assertTrue(listaObtida.contains(itemOfProduct3));
+
+        assertEquals(1L, listaObtida.get(0).getId());
+        assertEquals(1L, listaObtida.get(2).getId());
+    }
+    @Test
+    void testConvertItemOfProductDTO(){
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Laranja Lima");
+        product.setDescription("Laranja azedinha rica em vitamina c");
+
+        Seller seller = new Seller();
+        seller.setIdseller(1l);
+
+        PurchaseOrder purchaseOrder = new PurchaseOrder();
+        purchaseOrder.setId(1L);
+
+        SalesAd salesAd = new SalesAd();
+        salesAd.setVolume(500.0F);
+        salesAd.setMinimumTemperature(8F);
+        salesAd.setMaximumTemperature(45.0F);
+        salesAd.setPrice(1200.0D);
+        salesAd.setId(1L);
+        salesAd.setProduct(product);
+        salesAd.setSeller(seller);
+
+        SalesAd salesAd2 = new SalesAd();
+        salesAd2.setVolume(500.0F);
+        salesAd2.setMinimumTemperature(8F);
+        salesAd2.setMaximumTemperature(45.0F);
+        salesAd2.setPrice(1200.0D);
+        salesAd2.setId(2L);
+        salesAd2.setProduct(product);
+        salesAd2.setSeller(seller);
+
+        SalesAd salesAd3 = new SalesAd();
+        salesAd3.setVolume(500.0F);
+        salesAd3.setMinimumTemperature(8F);
+        salesAd3.setMaximumTemperature(45.0F);
+        salesAd3.setPrice(1200.0D);
+        salesAd3.setId(3L);
+        salesAd3.setProduct(product);
+        salesAd3.setSeller(seller);
+
+        List<ItemOfProduct> itemOfProducts = new ArrayList<>();
+
+        ItemOfProduct itemOfProduct = new ItemOfProduct(3200L,salesAd,purchaseOrder);
+        ItemOfProduct itemOfProduct2 = new ItemOfProduct(500L,salesAd2,purchaseOrder);
+        ItemOfProduct itemOfProduct3 = new ItemOfProduct(1100L,salesAd3,purchaseOrder);
+
+        itemOfProducts.add(itemOfProduct);
+        itemOfProducts.add(itemOfProduct2);
+        itemOfProducts.add(itemOfProduct3);
+
+        List<ItemOfProductDTO> itemOfProductDTOList =
+                itemOfProductService.convertItemOfProductDTO(itemOfProducts);
+
+        assertNotNull(itemOfProductDTOList);
+        assertEquals(1100L, itemOfProductDTOList.get(2).getQuantity());
+        assertEquals(3L, itemOfProductDTOList.get(2).getIdSalesAd());
 
     }
+
 }
