9b52b72343c7513e568596476316d39e15743191
pedroallima <pedro.leao@mercadolivre.com>
2021-11-17 10:10:03 -0300

    Refatorando requistos 4.1 e 2.2

--- src/main/java/com/mercadolibre/demo/repository/SectionRepository.java
+++ src/main/java/com/mercadolibre/demo/repository/SectionRepository.java
@@ -1,129 +1,29 @@
-package com.mercadolibre.demo.service;
+package com.mercadolibre.demo.repository;
 
-import com.mercadolibre.demo.dto.*;
-import com.mercadolibre.demo.dto.response.WareHouseProductItensDTO;
-import com.mercadolibre.demo.dto.response.WareHouseProductListDTO;
-import com.mercadolibre.demo.model.*;
-import com.mercadolibre.demo.repository.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.mercadolibre.demo.model.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.*;
+import java.util.List;
 
-@Service
-public class SectionService {
+@Repository
+public interface SectionRepository extends JpaRepository<Section,Long> {
 
+	//@Query(nativeQuery = true, value = "SELECT s.* from item_of_product iop, sales_ad sa, batch_stock bs, inbound_order io, section s ,products p, ware_house wh where iop.idsales_ad = sa.idsales_ad AND sa.idsales_ad = bs.idsales_ad AND bs.idbatch_number = io.idbatch_number AND io.section_code = s.section_code AND p.idproduct = sa.idproduct AND s.id_ware_house = wh.id_ware_house AND  upper(trim(s.category)) like %?1%")
+	// @Query(value = "select sec.category, bs.currentQuantity, s.price, w.wareHouseName, p.name  from WareHouse w, Section sec, InboundOrder i, BatchStock bs, SalesAd s,  Product p where s.product.id = p.id and bs.idSalesAd.id = s.id and i.batchStock.idBatchNumber = bs.idBatchNumber and i.section.idSection = sec.idSection and sec.WareHouse.idWareHouse = w.idWareHouse and sec.category like %?1%")
+	@Query(nativeQuery = true, value = "select sec.category, bs.current_quantity, s.price, w.ware_house_name, p.name  from ware_house w, section sec, inbound_order i, batch_stock bs, sales_ad s,  products p where s.idproduct = p.idproduct and bs.idsales_ad = s.idsales_ad and i.idbatch_number = bs.idbatch_number and i.section_code = sec.section_code and sec.id_ware_house = w.id_ware_house and sec.category like %?1%")
+	List<SectionByProducts> CategoryContaining(String name);
 
-	private SectionRepository sectionRepository;
-	private WareHouseRepository wareHouseRepository;
-	private InboundOrderRepository inboundOrderRepository;
+	public interface SectionByProducts {
+		String getCategory();
 
-	@Autowired
-	public SectionService(SectionRepository sectionRepository, WareHouseRepository wareHouseRepository,
-			InboundOrderRepository inboundOrderRepository, BatchStockRepository batchStockRepository,
-			SalesAdRepository salesAdRepository) {
-		this.sectionRepository = sectionRepository;
-		this.wareHouseRepository = wareHouseRepository;
-		this.inboundOrderRepository = inboundOrderRepository;
+		Long getCurrent_quantity();
+
+		Double getPrice();
+
+		String getWare_house_name();
+
+		String getName();
 	}
-
-	public Section save(SectionDTO dto) throws Exception {
-		Section section = convertSectionToDTO(dto);
-		return sectionRepository.save(section);
-	}
-
-	public List<Section> list() {
-		return sectionRepository.findAll();
-	}
-
-	public List<SectionTypeDTO>findSectionCategories(String category){
-		List<SectionRepository.SectionByProducts>  sectionByProducts = sectionRepository.CategoryContaining(category);
-		List<SectionTypeDTO> sectionTypeDTOS = new ArrayList<>();
-		for (SectionRepository.SectionByProducts section: sectionByProducts) {
-			SectionTypeDTO dto = SectionTypeDTO.builder()
-					.name(section.getCategory())
-					.quantity(section.getCurrent_quantity())
-					.price(section.getPrice())
-					.wareHouse(section.getWare_house_name())
-					.nameProduct(section.getName())
-					.build();
-			sectionTypeDTOS.add(dto);
-		}
-		return sectionTypeDTOS;
-	}
-
-	public WareHouseProductItensDTO listProduct(Long idProduct) {
-		List<WareHouse> idWarehouse = wareHouseRepository.findAll();
-
-		WareHouseProductItensDTO requisiteFour = new WareHouseProductItensDTO();
-		List<WareHouseProductListDTO> wareHouseProductListDTO = new ArrayList<>();
-		requisiteFour.setIdProduct(idProduct);
-
-		for (WareHouse itemWareHOuse : idWarehouse) {
-			WareHouseProductListDTO wareHouseProductDTO = new WareHouseProductListDTO();
-			List<InboundOrderRepository.StockByWareHouse> stockByWareHouses = inboundOrderRepository.buscaCanseira(idProduct,itemWareHOuse.getIdWareHouse());
-			wareHouseProductDTO.setQuantity(0L);
-			for (InboundOrderRepository.StockByWareHouse item : stockByWareHouses) {
-				wareHouseProductDTO.setWareHouseName(item.getWare_house_name());
-				wareHouseProductDTO.setQuantity(wareHouseProductDTO.getQuantity() + item.getCurrent_quantity());
-			}
-			if(stockByWareHouses.size()>0) {
-				wareHouseProductListDTO.add(wareHouseProductDTO);
-			}
-		}
-		requisiteFour.setList(wareHouseProductListDTO);
-		return requisiteFour;
-
-	}
-
-	public Optional<WareHouse> getWareHouse(SectionDTO dto){
-		Optional<WareHouse> wareHouse = wareHouseRepository.findById(dto.getIdWareHouse());
-		return wareHouse;
-	}
-	
-	public Optional<Section> findById(Long id) {
-		return sectionRepository.findById(id);
-	}
-
-	public Section update(SectionDTO dto, Long id) throws Exception {
-
-		Optional<Section> existSection = findById(id);
-		if (existSection.isPresent()) {
-			Section section = convertSectionToDTO(dto);
-			section.setIdSection(id);
-			return sectionRepository.saveAndFlush(section);
-		} else {
-			throw new Exception("Id não cadastrado");
-		}
-	}
-
-	public void delete(Long id) {
-		sectionRepository.deleteById(id);
-	}
-
-	public Section convertSectionToDTO(SectionDTO dto) throws Exception {
-		if (getWareHouse(dto).isPresent()) {
-			return new Section(dto.getCapacity(), dto.getCategory(), getWareHouse(dto).get());
-		} else {
-			throw new Exception("Id não cadastrado");
-		}
-	}
-
-
-/*	public List<SectionRepository.SectionByProducts> sectionByProducts (String name) {
-		List<SectionRepository.SectionByProducts>  sectionByProducts = sectionRepository.CategoryContaining(name);
-		return sectionByProducts;
-	}*/
-
-
-/*	public SectionTypeDTO setSectionTypeDTO(String category,Long quantity, Double price, String wareHouseName, String productName){
-		SectionTypeDTO sectionTypeDTO = new SectionTypeDTO();
-		sectionTypeDTO.setName(category);
-		sectionTypeDTO.setQuantity(quantity);
-		sectionTypeDTO.setPrice(price);
-		sectionTypeDTO.setWareHouse(wareHouseName);
-		sectionTypeDTO.setNameProduct(productName);
-
-		return sectionTypeDTO;
-	}*/
 }
