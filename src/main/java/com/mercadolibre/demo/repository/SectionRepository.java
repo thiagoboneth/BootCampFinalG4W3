66a52b424b8adcfbdeedbfafad80d7b55933af2d
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-17 09:07:12 -0300

    Refatoração de código

--- src/main/java/com/mercadolibre/demo/repository/SectionRepository.java
+++ src/main/java/com/mercadolibre/demo/repository/SectionRepository.java
@@ -12,8 +12,7 @@ import java.util.List;
 @Repository
 public interface SectionRepository extends JpaRepository<Section,Long> {
 
-    //@Query(nativeQuery = true, value = "SELECT s.* from item_of_product iop, sales_ad sa, batch_stock bs, inbound_order io, section s ,products p, ware_house wh where iop.idsales_ad = sa.idsales_ad AND sa.idsales_ad = bs.idsales_ad AND bs.idbatch_number = io.idbatch_number AND io.section_code = s.section_code AND p.idproduct = sa.idproduct AND s.id_ware_house = wh.id_ware_house AND  upper(trim(s.category)) like %?1%")
-    //@Query(value = "SELECT s.sectionCode, s.capacity, s.category, s.idWareHouse.idWareHouse from ItemOfProduct iop, SalesAd sa, BatchStock bs, InboundOrder io, Section s ,Product p, WareHouse wh where iop.salesAd.id = sa.id AND sa.id = bs.salesad.id AND bs.batchNumber = io.batchStock.batchNumber AND io.section.sectionCode = s.sectionCode AND p.id = sa.product.id AND s.idWareHouse.idWareHouse = wh.idWareHouse AND  upper(trim(s.category)) like %?1%")
+
     @Query(nativeQuery = true, value = "SELECT s.* from  section s where upper(trim(s.category)) like %?1%")
     List<Section> buscarPorSessao(String name);
 
