c17a3ca71b90aeac8ca0d3e709b2539c5b38bb84
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-18 19:05:54 -0300

    Implementado testes de controller SellerControllerTest

--- src/test/java/com/mercadolibre/demo/controller/SellerControllerTest.java
+++ src/test/java/com/mercadolibre/demo/controller/SellerControllerTest.java
@@ -1,115 +1,129 @@
 package com.mercadolibre.demo.controller;
 
 import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.net.URI;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercadolibre.demo.dto.TokenDTO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import javax.transaction.Transactional;
+
+@Transactional
 @AutoConfigureTestEntityManager
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SellerControllerTest {
-	
+
 	private URI uri;
-	private MockHttpServletRequestBuilder request;
-	private ResultMatcher expectedResult;
-	
-	@Autowired
-	private TestEntityManager entityManager;
-	
+
 	@Autowired
 	MockMvc mockMvc;
-	
-	@Test
-	public void testListBuyerGetAll() throws Exception {
-		
-		uri = new URI("/api/v1/fresh-products/seller/list");
-		
-		assertNotNull(uri);
 
-		request = MockMvcRequestBuilders.get(uri);
-		expectedResult = MockMvcResultMatchers.status().isOk();
+	private TokenDTO tokenDTO;
 
-		String response = mockMvc.perform(request).andExpect(expectedResult).andReturn().getResponse()
-				.getContentAsString();
+	@BeforeEach
+	public void testandoAutenticacao() throws Exception {
+		String json = "{\"user\": \"thiago\", \"senha\": \"123\"}";
+		uri = new URI("/auth");
 
-		assertNotNull(response);
+		MvcResult resultContendoToken = mockMvc
+				.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON).content(json))
+				.andExpect(status().isOk()).andReturn();
+		tokenDTO = new ObjectMapper().readValue(resultContendoToken.getResponse().getContentAsString(), TokenDTO.class);
 	}
 	
 	@Test
-	public void testSaveBuyerWithSuccess() throws Exception {
+	public void testsaveSeller() throws Exception {
 		
 		uri = new URI("/api/v1/fresh-products/seller/save");
 		
 		assertNotNull(uri);
 
-		String json = "{\"name\": \"Ana Maria\", \"lastname\": \"Braga\"}";
+		String requestJson =  "{\"name\": \"Joao\", \"lastname\": \"Roberto\"}";
 
-		request = MockMvcRequestBuilders.post(uri).content(json).header("Content-Type", "application/json");
-				
-		expectedResult = MockMvcResultMatchers.status().isCreated();
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.post(uri)
+						.content(requestJson)
+						.header("Content-Type", "application/json")
+						.header("Authorization", tokenDTO.getTipo() + " " + tokenDTO.getToken()))
+				.andExpect(status().isCreated()).andReturn();
 
-		mockMvc.perform(request).andExpect(expectedResult);
-		
+		String responseJson = result.getResponse().getContentAsString();
+
+		assertNotNull(responseJson);
 	}
-	
+
+	@Test
+	public void testlistSeller() throws Exception {
+
+		uri = new URI("/api/v1/fresh-products/seller/list");
+
+		assertNotNull(uri);
+
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.get(uri).header("Authorization", tokenDTO.getTipo() + " " + tokenDTO.getToken()))
+				.andExpect(status().isOk()).andReturn();
+
+		String jsonRetorno = result.getResponse().getContentAsString();
+
+		assertNotNull(jsonRetorno);
+
+	}
+
+	@Test
+	public void testupdateSeller() throws Exception{
+
+		uri = new URI("/api/v1/fresh-products/seller/update/1");
+
+		assertNotNull(uri);
+
+		String requestJson =  "{\"name\": \"Joao\", \"lastname\": \"Roberto\"}";
+
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.put(uri)
+						.content(requestJson)
+						.header("Content-Type", "application/json")
+						.header("Authorization", tokenDTO.getTipo() + " " + tokenDTO.getToken()))
+				.andExpect(status().isCreated()).andReturn();
+
+		String responseJson = result.getResponse().getContentAsString();
+
+		assertNotNull(responseJson);
+	}
+
 	@Test
 	public void testUpdateBuyerWithSuccess() throws Exception {
-		
-		uri = new URI("/api/v1/fresh-products/seller/update/1");
-		
-		assertNotNull(uri);
-		
-		String json = "{\"name\": \"Mariana\", \"lastname\": \"Rios\"}";
 
-		request = MockMvcRequestBuilders.put(uri).content(json).header("Content-Type", "application/json");
-				
-		expectedResult = MockMvcResultMatchers.status().isCreated();
 
-		mockMvc.perform(request).andExpect(expectedResult);
-		
-	}
-	
-	@Test
-	public void testSaveBuyerNoSuccesss() throws Exception {
-		
-		uri = new URI("/api/v1/fresh-products/seller/save");
-		
+		uri = new URI("/api/v1/fresh-products/seller/delete/1");
+
 		assertNotNull(uri);
 
-		String json = "{\"name\": null, \"lastname\": \"\"}";
 
-		request = MockMvcRequestBuilders.post(uri).content(json).header("Content-Type", "application/json");
-				
-		expectedResult = MockMvcResultMatchers.status().isBadRequest();
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.delete(uri)
+						.header("Authorization", tokenDTO.getTipo() + " " + tokenDTO.getToken()))
+				.andExpect(status().isOk()).andReturn();
 
-		mockMvc.perform(request).andExpect(expectedResult);
-	}
-	
-	
-	@Test
-	public void testDeleteBuyer() throws Exception {
-		
-		uri = new URI("/api/v1/fresh-products/seller/delete/8");
-		
-		assertNotNull(uri);
+		String responseJson = result.getResponse().getContentAsString();
 
-		request = MockMvcRequestBuilders.delete(uri).header("Content-Type", "application/json");
-				
-		expectedResult = MockMvcResultMatchers.status().isOk();
-
-		mockMvc.perform(request).andExpect(expectedResult);
+		assertNotNull(responseJson);
 	}
 
 }
