7e41266f167424ebb8bb2c10df70df8fb673c2bd
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-19 21:50:57 -0300

    Implementação interface para uso do swagger

--- src/main/java/com/mercadolibre/demo/controller/BatchStockController.java
+++ src/main/java/com/mercadolibre/demo/controller/BatchStockController.java
@@ -2,6 +2,7 @@ package com.mercadolibre.demo.controller;
 
 import java.util.List;
 
+import com.mercadolibre.demo.config.SecurityController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/fresh-products/batchstock")
-public class BatchStockController {
+public class BatchStockController implements SecurityController {
 
     @Autowired
     private BatchStockService batchStockService;
