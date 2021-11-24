7184eba4dbbf06551d14e1810bc9f193c0c1b080
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-23 22:48:55 -0300

    Implementação de metodo de gerador numero de boleto e conversor de dto para boleto

--- src/main/java/com/mercadolibre/demo/service/PaymentsService.java
+++ src/main/java/com/mercadolibre/demo/service/PaymentsService.java
@@ -1,10 +1,10 @@
 package com.mercadolibre.demo.service;
 
 import com.mercadolibre.demo.dto.PaymentDTO;
+import com.mercadolibre.demo.dto.PaymentDTOBoleto;
 import com.mercadolibre.demo.model.Payment;
 import com.mercadolibre.demo.model.PaymentStatus;
 import com.mercadolibre.demo.model.PurchaseOrder;
-import com.mercadolibre.demo.repository.ItemOfProductRepository;
 import com.mercadolibre.demo.repository.PaymentRepository;
 import com.mercadolibre.demo.repository.PurchaseOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,64 +12,64 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-import static com.mercadolibre.demo.model.PaymentStatus.EM_ANALISE;
-
-
 @Service
 public class PaymentsService {
 
     private PaymentRepository paymentRepository;
     private PurchaseOrderRepository purchaseOrderRepository;
-    private ItemOfProductRepository itemOfProductRepository;
 
     @Autowired
-    public PaymentsService(PaymentRepository paymentRepository, PurchaseOrderRepository purchaseOrderRepository, ItemOfProductRepository itemOfProductRepository) {
+    public PaymentsService(PaymentRepository paymentRepository, PurchaseOrderRepository purchaseOrderRepository) {
         this.paymentRepository = paymentRepository;
         this.purchaseOrderRepository = purchaseOrderRepository;
-        this.itemOfProductRepository = itemOfProductRepository;
     }
 
     public Payment payment(Long idCart, PaymentStatus paymentStatus, Long installment) throws Exception {
         Optional<PurchaseOrder> existCart = findById(idCart);
-        PaymentDTO paymentDTO = new PaymentDTO();
-        if (existCart.isPresent()){
+        if (existCart.isPresent()) {
+            PaymentDTO paymentDTO;
+            PaymentDTOBoleto paymentDTOBoleto;
             switch (paymentStatus) {
                 case PARCELADO:
-                    paymentDTO = rentDTO(divDB(idCart,installment), paymentStatus, installment);
+                    paymentDTO = rentDTO(divDB(idCart, installment), paymentStatus, installment);
                     return paymentRepository.save(convertPaymentoToDTO(paymentDTO));
                 case BOLETO:
+                    paymentDTOBoleto = rentDTOBoleto(sumDB(idCart),paymentStatus, installment, ramdomNumbers());
+                    return paymentRepository.save(convertPaymentoToDTOBoleto(paymentDTOBoleto));
+                case EM_ANALISE:
                     paymentDTO = rentDTO(sumDB(idCart), paymentStatus, installment);
                     return paymentRepository.save(convertPaymentoToDTO(paymentDTO));
-                case 3:
-                    System.out.println("Wednesday");
-                    break;
-                case 4:
-                    System.out.println("Thursday");
-                    break;
-                case 5:
-                    System.out.println("Friday");
-                    break;
-                case 6:
-                    System.out.println("Saturday");
-                    break;
-                case 7:
-                    System.out.println("Sunday");
-                    break;
-//                return payment;
-            }else if (paymentStatus.equals(EM_ANALISE)) {
-                PaymentDTO paymentDTO = rentDTO(sumDB(idCart), paymentStatus, installment);
-                Payment dto = convertPaymentoToDTO(paymentDTO);
-                Payment payment = paymentRepository.save(dto);
-                return payment;
+                case CREDITO_A_VISTA:
+                    paymentDTO = rentDTO(sumDB(idCart), paymentStatus, installment);
+                    return paymentRepository.save(convertPaymentoToDTO(paymentDTO));
+                case CANCELADO:
+                    paymentDTO = rentDTO(sumDB(idCart), paymentStatus, installment);
+                    return paymentRepository.save(convertPaymentoToDTO(paymentDTO));
+                default:
+                    throw new IllegalStateException("Error: " + paymentStatus);
             }
-
-            }else {
-              throw new Exception("Carrinho inexistente");
+        }else {
+                    throw new Exception("Carrinho inexistente");
         }
     }
 
     public Payment convertPaymentoToDTO(PaymentDTO payment){
         return new Payment(payment.getPaymentStatus(),payment.getValueOfCart(),payment.getInstallment());
+    }
+
+    public Payment convertPaymentoToDTOBoleto(PaymentDTOBoleto payment){
+        return new Payment(payment.getPaymentStatus(),payment.getValueOfCart(),payment.getInstallment(),payment.getNumeroBoleto());
+    }
+
+    public String ramdomNumbers(){
+        String validChar = "1234567890";
+        String sequenceInit = "";
+        char finalSequence;
+        for (int k = 0;k<48;k++){
+            finalSequence=validChar.charAt((int) Math.floor(Math.random()*validChar.length()));
+            sequenceInit+=Character.toString(finalSequence);
+        }
+        return sequenceInit;
     }
 
     public Optional<PurchaseOrder> findById(Long id) {
@@ -88,5 +88,9 @@ public class PaymentsService {
 
     private PaymentDTO rentDTO(Double sumDB, PaymentStatus paymentStatus, Long installment){
         return new PaymentDTO(paymentStatus,sumDB,installment);
+    }
+
+    private PaymentDTOBoleto rentDTOBoleto(Double sumDB, PaymentStatus paymentStatus,Long installment, String boletoNumero){
+        return new PaymentDTOBoleto(paymentStatus,sumDB,installment,boletoNumero);
     }
 }
