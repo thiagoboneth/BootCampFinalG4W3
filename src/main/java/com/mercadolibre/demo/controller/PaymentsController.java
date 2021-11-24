51a69529cc99c15549e82abc23740258df262a79
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-24 12:54:22 -0300

    Controller implementado

--- src/main/java/com/mercadolibre/demo/controller/PaymentsController.java
+++ src/main/java/com/mercadolibre/demo/controller/PaymentsController.java
@@ -1,18 +1,17 @@
 package com.mercadolibre.demo.controller;
 
 import com.mercadolibre.demo.config.SecurityController;
+import com.mercadolibre.demo.dto.PaymentDTO;
 import com.mercadolibre.demo.model.Payment;
 import com.mercadolibre.demo.model.PaymentStatus;
 import com.mercadolibre.demo.service.PaymentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/paymentMethod/")
@@ -27,5 +26,18 @@ public class PaymentsController implements SecurityController {
     public ResponseEntity<Payment> payment(@Valid Long idCart, PaymentStatus paymentStatus, Long installment) throws Exception {
         Payment payment = paymentsService.payment(idCart, paymentStatus, installment);
         return new ResponseEntity<>(payment, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping(value = "/listAllPayments")
+    @ResponseBody
+    public ResponseEntity<List<Payment>> listAllPayments() {
+        List<Payment> payments = paymentsService.listAllPayments();
+        return new ResponseEntity<>(payments, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/updatePaymentStatus/{id}")
+    public ResponseEntity<Payment> updatePaymentStatus(@Valid @RequestBody PaymentDTO paymentDTO, @PathVariable Long id) throws Exception {
+        Payment payment = paymentsService.updatePaymentStatus(paymentDTO, id);
+        return new ResponseEntity<>(payment, HttpStatus.GONE);
     }
 }
