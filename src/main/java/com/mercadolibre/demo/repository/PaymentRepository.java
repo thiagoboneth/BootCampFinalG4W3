d3dcff938ffe0ce4d751c41261eacdd0983add1b
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-24 09:32:24 -0300

    Query implementada para somar mais de um pedido com quantidade difrente no carrinho

--- src/main/java/com/mercadolibre/demo/repository/PaymentRepository.java
+++ src/main/java/com/mercadolibre/demo/repository/PaymentRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
-    @Query(value = "SELECT iop.salesAd.price FROM ItemOfProduct iop WHERE iop.purchaseOrder.id =?1")
+    @Query(value = "SELECT SUM(iop.salesAd.price) FROM ItemOfProduct iop WHERE iop.purchaseOrder.id =?1")
     Double price (Long name);
 
     @Query(value = "SELECT SUM(iop.quantity) FROM ItemOfProduct iop WHERE iop.purchaseOrder.id =?1")
