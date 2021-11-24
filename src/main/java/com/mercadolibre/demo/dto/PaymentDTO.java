8858496821daf1431af36ac7c1064fe8694eb4fe
thiagoboneth <thiago.luis@mercadolivre.com>
2021-11-24 17:37:41 -0300

    Implementação de lombok na classe

--- src/main/java/com/mercadolibre/demo/dto/PaymentDTO.java
+++ src/main/java/com/mercadolibre/demo/dto/PaymentDTO.java
@@ -3,12 +3,14 @@ package com.mercadolibre.demo.dto;
 import com.mercadolibre.demo.model.PaymentStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PaymentDTO {
 
     private PaymentStatus paymentStatus;
